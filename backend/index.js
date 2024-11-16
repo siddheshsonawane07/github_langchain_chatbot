848b0d158377766ba01963697bef3e1ccd12a923
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2024-11-16 22:17:05 +0530

    individual repo

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 import express, { json } from 'express';
 import { Octokit } from 'octokit';
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config(); 
 
 const app = express();
 const port = 3000;
-const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN }); 
-
+const octokit = new Octokit({auth : process.env.GITHUB_AUTH_TOKEN}); 
+console.log(process.env.GITHUB_AUTH_TOKEN)
 app.use(json());
 
 app.get('/', (req, res) => {
@@ -16,9 +16,25 @@ app.get('/', (req, res) => {
 
 app.get('/repos', async (req, res) => {
   try {
-    const { data } = await octokit.request('GET /users/{username}/', {
-      username: 'siddheshsonawane07' 
+    const { data } = await octokit.request('GET /repos/{owner}/', {
+      owner: 'siddheshsonawane07' 
     });
+    res.json(data); 
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch repositories' });
+  }
+});
+
+app.get('/repo', async (req, res) => {
+  try {
+    const { data } = await octokit.request('GET /repos/{owner}/{repo}', {
+      owner: 'siddheshsonawane07',
+      repo: 'proctorise',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
     res.json(data); 
   } catch (error) {
     console.error(error);
